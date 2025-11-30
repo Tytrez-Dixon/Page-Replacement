f741ca0e57e5ae14c9c0762438bd7d8d98f4b108
Tytrez-Dixon <dixontytrez@gmail.com>
2025-11-30 12:51:39 -0500

    Preliminary functionality implemented.
    Implemented process frame size selection and page selection.

--- PageReplacement.java
+++ PageReplacement.java
@@ -3,7 +3,7 @@
  * 
  * Date Created: 11/27/2025
  * 
- * Date Updated: __________
+ * Date Updated: 11/30/2025
  * 
  * This Java program simulates the page replacement OS operation using 3, 4,
  * or 5 frames. The FCFS (Cannon Miles) and LRU (Tytrez Dixon) page 
@@ -21,19 +21,20 @@ public class PageReplacement {
 		
 		int frameCount = 0;
 		
+		Scanner input = new Scanner(System.in);
+		
 		while(true) {
-			Scanner input = new Scanner(System.in);
 			
-			int frameCount1 = input.nextInt();
+			int frameCount2 = input.nextInt();
 				
 			// Reject inputs that do not fall into the specified range.
-			if (frameCount1 < 3 || frameCount1 > 5) {
+			if (frameCount2 < 3 || frameCount2 > 5) {
 				System.out.println("Invalid Input. Please select a valid " +
-						"frame count:");
+						"frame count.");
 			}
 			
 			else {
-				frameCount = frameCount1;
+				frameCount = frameCount2;
 				break;
 			}
 		}	
@@ -41,11 +42,26 @@ public class PageReplacement {
 		// Create process of specified frame count.
 		int[] process = new int[frameCount];
 
+		// Prompt user to enter page requests based on process size
+		System.out.println("Enter page requests. Please enter exactly" +
+				" twice as many pages as the number of frames you selected.");
+		
+		// Create array to keep track of page requests.
+		int[] pageRequests = new int[frameCount * 2];
+		int processIndex = 0;
+		
+		while (input.hasNextInt()) {
+			pageRequests[processIndex] = input.nextInt();
+			processIndex++;
+			
+			if (processIndex == pageRequests.length) {
+				break;
+			}
+		}
+		
+		input.close();
 		
 		
-//		for (int x: process) {
-//			System.out.println(x);
-//		}
 	}
 
 }
